893c2561d40d9e4f07969d2b79941b990074254b
wujingm1n <1254568217@qq.com>
2022-09-13 01:50:33 +0800

    1

---
+++ 数据爬虫可视化大屏/Tools.py
@@ -0,0 +1,28 @@
+# 设置
+def jupyter_settings(style='darkgrid', context='notebook', palette=["#39A7D0","#36ADA4"], font_name='SimHei'):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    import seaborn as sns
+    # %config IPCompleter.greedy=True
+    sns.set()
+    # %matplotlib inline
+    # %config Inlinebackend.figure_format = 'svg'
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.rcParams['font.sans-serif'] = font_name
+    sns.set_style(style=style)
+    sns.set_context(context=context)
+    sns.set_palette(palette=palette)
+
+# 忽略警告
+def ignore_warnings():
+    import warnings
+    warnings.filterwarnings('ignore')
+
+
+def pyecharts():
+    import numpy as np
+    import pandas as pd
+    from pyecharts.charts import *
+    from pyecharts import options as opts
+    from pyecharts.globals import ThemeType 
